eb12793ec5bb74f61e4e5787c1da76e8fa31a418
Giuliano De Rossi <derossi.giuliano@gmail.com>
2016-09-30 11:31:19 +0200

    FB app test 4

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 $fb = new Facebook\Facebook([
   'app_id' => '781751771966575',
   'app_secret' => 'c5b6e0ebaa233b5c018e4e38b177ff82',
-  'default_graph_version' => 'v2.5',
+  'default_graph_version' => 'v2.7',
 ]);
 
 $helper = $fb->getRedirectLoginHelper();
